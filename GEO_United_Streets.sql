8a1e6e4892751f57a298fd3766687898202aeda0
andrehuller <andrehuller@gmail.com>
2017-06-12 10:22:31 -0300

    Create GEO_United_Streets.sql

--- GEO_United_Streets.sql
+++ GEO_United_Streets.sql
@@ -17,7 +17,6 @@ DECLARE	cursor_connected CURSOR FOR
 	var_united_id INTEGER;
 BEGIN
 	-- Remove os dados anteriores --
-
 	DELETE	FROM streetbase.tb_geo_eixos_viarios_unidos_pol_p4674
 	WHERE	city_id = a_city_id;
 
@@ -27,11 +26,9 @@ BEGIN
 	AND	united_id IS NOT NULL;
 
 	DELETE	FROM streetbase.tb_united_street;
-
 	DELETE	FROM streetbase.tb_united_streets;
 
 	-- Constroi uma lista de adjacência para os trechos conectados --
-
 	INSERT	INTO streetbase.tb_united_streets(id1, id2, edge, geom)
 	SELECT	t1.id, t2.id, ARRAY[t1.id, t2.id], ST_Intersection(t1.geom, t2.geom)
 	FROM	streetbase.tb_geo_eixos_viarios_conectados_pol_p4674 t1,
@@ -48,17 +45,15 @@ BEGIN
 	AND	ST_GeometryType(ST_Intersection(t1.geom, t2.geom)) = 'ST_Point'; --ST_MultiPoint IN SOME CASES
 
 	-- Remove bifurcações --
-
 	DELETE	FROM streetbase.tb_united_streets t
 	WHERE	t.id IN       (	SELECT	t1.id
 				FROM	streetbase.tb_united_streets t1,
 					streetbase.tb_united_streets t2
 				WHERE	ST_Equals(t1.geom, t2.geom)
-				AND	t1.edge && t2.edge --A OVERLAPS B BUT A DOES NOT CONTAIN B
+				AND	t1.edge && t2.edge --A overlaps B but A does not contain B
 				AND	NOT t1.edge @> t2.edge );
 
 	-- Coloca cada trecho em um grupo --
-
 	FOR var_record IN cursor_connected LOOP
 		SELECT	group_id
 		INTO	var_group_id1
@@ -85,7 +80,6 @@ BEGIN
 		ELSIF var_group_id1 IS NOT NULL THEN
 			INSERT	INTO streetbase.tb_united_street(id, group_id)
 			VALUES	(var_record.id2, var_group_id1);
-
 		ELSE
 			INSERT	INTO streetbase.tb_united_street(id, group_id)
 			VALUES	(var_record.id1, var_group_id2);
@@ -93,7 +87,6 @@ BEGIN
 	END LOOP;
 
 	-- Coloca cada trecho sem conexão alguma em um grupo próprio --
-
 	FOR var_record IN cursor_separated LOOP
 		INSERT	INTO streetbase.tb_united_street(id, group_id)
 		VALUES	(var_record.id, var_group_id);
@@ -102,7 +95,6 @@ BEGIN
 	END LOOP;
 
 	-- Constroi o id do trecho unido a partir do grupo --
-
 	SELECT	COALESCE(MAX(id), 0) + 1
 	INTO	var_united_id
 	FROM	streetbase.tb_geo_eixos_viarios_unidos_pol_p4674;
@@ -125,7 +117,6 @@ BEGIN
 		st_type, st_title, st_name, alt_st_name;
 
 	DELETE	FROM streetbase.tb_united_street;
-
 	DELETE	FROM streetbase.tb_united_streets;
 
 	--GEO_Update_Osm_Name
@@ -170,12 +161,45 @@ BEGIN
 	WHERE	city_id = a_city_id
 	AND	streetbase.GEO_Street_Reversed(geom, st_name);
 
-	UPDATE	streetbase.tb_geo_eixos_viarios_separados_pol_p4674 t
-	SET	geom = ST_Reverse(t.geom)
-	FROM  (	SELECT	(ST_Dump(geom)).geom
+	UPDATE	streetbase.tb_geo_eixos_viarios_conectados_pol_p4674 t1
+	SET	geom = ST_Reverse(t1.geom)
+	FROM  (	SELECT	id, (ST_Dump(geom)).geom
 		FROM	streetbase.tb_geo_eixos_viarios_unidos_pol_p4674
-		WHERE	city_id = a_city_id ) t1
-	WHERE	ST_Contains(t1.geom, t.geom)
-	AND	ST_Line_Locate_Point(t1.geom, ST_StartPoint(t.geom)) > ST_Line_Locate_Point(t1.geom, ST_EndPoint(t.geom));
+		WHERE	city_id = a_city_id ) t2
+	WHERE	t1.united_id = t2.id
+	AND	ST_Line_Locate_Point(t2.geom, ST_StartPoint(t1.geom)) > ST_Line_Locate_Point(t2.geom, ST_EndPoint(t1.geom));
+
+	UPDATE	streetbase.tb_geo_eixos_viarios_separados_pol_p4674 t1
+	SET	geom = ST_Reverse(t1.geom)
+	FROM  (	SELECT	id, (ST_Dump(geom)).geom
+		FROM	streetbase.tb_geo_eixos_viarios_conectados_pol_p4674
+		WHERE	city_id = a_city_id ) t2
+	WHERE	t1.connected_id = t2.id
+	AND	ST_Line_Locate_Point(t2.geom, ST_StartPoint(t1.geom)) > ST_Line_Locate_Point(t2.geom, ST_EndPoint(t1.geom));
+
+	--GEO_Update_Street_Numeration
+	--Atualiza a numeração dos trechos separados
+	WITH tb_street_number AS (
+		SELECT	ts.id, 
+			round(ST_LineLocatePoint(tc.geom, ST_StartPoint(ts.geom)) * (tc.max_num - tc.min_num) + tc.min_num) AS from_l,
+			round(ST_LineLocatePoint(tc.geom, ST_EndPoint(ts.geom)) * (tc.max_num - tc.min_num) + tc.min_num)
+				- (CASE WHEN NOT ST_Equals(ST_EndPoint(ts.geom), ST_EndPoint(tc.geom)) THEN 1 ELSE 0 END) AS to_l
+		FROM	streetbase.tb_geo_eixos_viarios_conectados_pol_p4674 tc,
+			streetbase.tb_geo_eixos_viarios_separados_pol_p4674 ts
+		WHERE	tc.city_id = a_city_id
+		AND	tc.min_num IS NOT NULL
+		AND	tc.max_num IS NOT NULL
+		AND	tc.min_num <> tc.max_num
+		AND	ts.city_id = a_city_id --not necessary, but with it postgresql becomes wise about choosing indexes
+		AND	ts.connected_id = tc.id
+		AND	LEAST(ts.from_l, ts.from_r) IS NULL
+		AND	GREATEST(ts.to_l, ts.to_r) IS NULL
+	)
+	UPDATE	streetbase.tb_geo_eixos_viarios_separados_pol_p4674 ts
+	SET	from_l = st_number.from_l,
+		to_l = st_number.to_l
+	FROM	tb_street_number st_number
+	WHERE	st_number.from_l <= st_number.to_l --in case there are less numbers than street segments.
+	AND	ts.id = st_number.id;
 END;
 $$ LANGUAGE plpgsql VOLATILE;
